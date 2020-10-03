cc679777fcdc6252fcf77cf6fb95fa82f8d3b957
Michael Dobrijevic <michael@Michaels-MacBook-Air.local>
2020-10-04 00:25:55 +1000

    Completed 59A

---
+++ 59A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string s;
+    int upperCount = 0;
+    int lowerCount = 0;
+    bool toCapitalize = false;
+    cin >> s;
+    int l = s.length();
+    for (int i = 0; i < l; i++) {
+        if (s[i] > 96) {
+            lowerCount++;
+        } else {
+            upperCount++;
+        }
+    }
+    if (lowerCount == upperCount || lowerCount > upperCount) {
+        
+        for (int i = 0; i < l; i++) {
+            if (s[i] < 96) {
+                s[i] = s[i] + 32;
+            }
+        }
+    }
+    if (lowerCount < upperCount) {
+        for (int i = 0; i < l; i++) {
+            if (s[i] > 96) {
+                s[i] = s[i] - 32;
+            }
+        }   
+    }
+    cout << s << endl;
+    return 0;
+}
